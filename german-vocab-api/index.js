333c51a666093769f5fc6d9c382043d6d28e2870
Katy <ihatetoast@gmail.com>
2021-08-23 15:49:44 -0500

    adding word search with id arg

--- german-vocab-api/index.js
+++ german-vocab-api/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
+// const { GraphQLScalarType } = require("graphql");
+// const { Kind } = require("graphql/language");
 
 const typeDefs = gql`
   enum Status {
@@ -6,16 +8,17 @@ const typeDefs = gql`
     INTERMEDIATE
     HARD
     RETIRED
+    UNKNOWN
   }
 
   type Example {
-    id: ID
-    sentence: String!
-    case: String
-    number: Int
-    dateAdded: String
+    id: ID!
+    sentence: String
   }
-
+  type Theme {
+    id: ID!
+    genre: String
+  }
   type Word {
     id: ID!
     german: String!
@@ -23,18 +26,64 @@ const typeDefs = gql`
     gender: String!
     pluralConst: String!
     pluralForm: String!
-    theme: String
-    example: [Example!]!
+    theme: [Theme]
+    example: [Example] # Valid: null, [], [...some data]. what is not valid: [...some data without name or id]
+    #example: [Example]! Valid: [], [...some data]
+    # example: [Example!]! Valid: [...some data]
     status: Status
     dateAdded: String
   }
-
+# type ID is always str
   type Query {
     words: [Word]
     word(id: ID): Word
   }
 `;
-
+const themes = [
+  {
+    id:"animals1",
+    genre: 'mammals'
+  },
+  {
+    id:"animals2",
+    genre: 'pets'
+  },
+  {
+    id:"animals3",
+    genre: 'fish'
+  },
+  {
+    id:"animals4",
+    genre: 'birds'
+  },
+  {
+    id:"animals5",
+    genre: 'reptiles & amphibians'
+  },
+  ,
+  {
+    id:"animals6",
+    genre: 'invertebrates'
+  }
+]
+const examples = [
+  {
+    id: "hund1",
+    sentence: "Der Hund läuft schnell",
+  },
+  {
+    id: "hund2",
+    sentence: "Ich sehe den Hund",
+  },
+  {
+    id: "schwein1",
+    sentence: "Das Schwein ißt viel.",
+  },
+  {
+    id: "katze1",
+    sentence: "Die Katze schläft immer.",
+  },
+];
 const words = [
   {
     id: "1234",
@@ -44,11 +93,8 @@ const words = [
     pluralConst: "-e",
     pluralForm: "Hunde",
     theme: "animals",
-    example: [
-      { sentence: "Der Hund läuft schnell", case: "nominative", number: 1 },
-      { sentence: "Ich sehe den Hund", case: "nominative", number: 1 },
-    ],
-    dateAdded: "08-18-2021",
+    example: [{ id: "hund1" }, { id: "hund2" }],
+    dateAdded: new Date("8-18-2021"),
   },
   {
     id: "1235",
@@ -58,8 +104,8 @@ const words = [
     pluralConst: "-en",
     pluralForm: "Katzen",
     theme: "animals",
-    example: [{ sentence: "Die Katze schläft immer." }],
-    dateAdded: "08-18-2021",
+    example: [{ id: "katze1" }],
+    dateAdded: new Date("10-12-1983"),
   },
   {
     id: "1236",
@@ -69,25 +115,29 @@ const words = [
     pluralConst: "-e",
     pluralForm: "Schweine",
     theme: "animals",
-    example: [{ sentence: "Das Schwein ißt viel." }],
+    example: [{ id: "Schwein1" }],
     dateAdded: "08-18-2021",
   },
 ];
+
 // returns static data
 const resolvers = {
   Query: {
     words: () => {
       return words;
     },
-    word: (obj, { id }, context, info) => {
-      const foundWord = words.find((word) =>{
+    word: (obj, { id }, context, info) =>{
+      const foundWord = words.find(word => {
         return word.id === id
       })
-      return foundWord;
-    },
+      return foundWord
+    }
   },
 };
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
